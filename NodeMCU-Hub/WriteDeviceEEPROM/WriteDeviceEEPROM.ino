406c2621c91cb3319a184b39e1e71b5f9ccec1b3
thiensu1122 <thiensuhoisinh2007@gmail.com>
2020-08-29 13:48:23 +0700

    done write EEPROM

--- NodeMCU-Hub/WriteDeviceEEPROM/WriteDeviceEEPROM.ino
+++ NodeMCU-Hub/WriteDeviceEEPROM/WriteDeviceEEPROM.ino
@@ -1,22 +1,32 @@
 #include <EEPROM.h>
 
+//DHT11(0, "DHT11 measure temp and humi"),
+//RGBLED(1, "Red green blue led");
 
 union UnsignedIntAsBytes {
 	uint16_t uival;
 	uint8_t bval[2];
 } unsignedIntAsBytes;
 
-void setup() {
-	uint16_t deviceID = 1;
-	uint8_t deviceCode = 1;
-	
-	
+void clearEEPROM() {
 	for (int i = 0 ; i < EEPROM.length() ; i++) {
 		EEPROM.write(i, 0);
 	}
-	for (int i =0 ; i< 3; i++) {
-		EEPROM.write(i, 0);
-	}
+}
+
+void setup() {
+	uint16_t deviceID = 1;
+	uint8_t deviceCode = 1;
+
+	unsignedIntAsBytes.uival = deviceID;
+	EEPROM.write(unsignedIntAsBytes.bval[1], 0);
+	EEPROM.write(unsignedIntAsBytes.bval[2], 1);
+	EEPROM.write(deviceCode, 2);
+	
+	
+
+	
+
 }
 
 void loop() {
